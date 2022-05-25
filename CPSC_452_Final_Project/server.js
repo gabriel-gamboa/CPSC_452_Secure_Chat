d8f1892fa54d5acc2c2db088cbba5b4eb3a12de6
gabriel-gamboa <gabygamboa@sbcglobal.net>
2022-05-24 23:26:43 -0700

    First upload

---
+++ CPSC_452_Final_Project/server.js
@@ -0,0 +1,24 @@
+const io = require('socket.io')(3000, {
+    cors: {
+      origin: "*"
+    }
+  })
+
+const users = {}
+
+io.on('connection', socket => {
+    //console.log('new User')
+    //socket.emit('chat-message', 'Hello World')
+    socket.on('new-user', name => {
+      users[socket.id] = name
+      socket.broadcast.emit('user-connected', name)
+    })
+    socket.on('send-chat-message', message =>{
+        socket.broadcast.emit('chat-message', {message: message, name: users[socket.id]})
+        //console.log(message)
+    })
+    socket.on('disconnect', () => {
+      socket.broadcast.emit('user-disconnected', users[socket.id])
+      delete users[socket.id]
+    })
+})
